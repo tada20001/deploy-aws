7a2789ddcf6cf309915b0e952ed4926d6dfc3cd6
tada <tada20001@gmail.com>
2019-01-07 16:15:17 +0900

    [EP03] 배포 - AWS Elastic Beanstalk - .ebextensions/ 추가

--- gallery_dev/settings/prod.py
+++ gallery_dev/settings/prod.py
@@ -6,12 +6,12 @@ DEBUG = False
 
 import os
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.postgresql'),
-        'HOST': os.environ.get('DATABASE_HOST', None),
-        'NAME': os.environ.get('DATABASE_NAME', None),
-        'USER': os.environ.get('DATABASE_USER', None),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD', None),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.postgresql'),
+#         'HOST': os.environ.get('DATABASE_HOST', None),
+#         'NAME': os.environ.get('DATABASE_NAME', None),
+#         'USER': os.environ.get('DATABASE_USER', None),
+#         'PASSWORD': os.environ.get('DATABASE_PASSWORD', None),
+#     }
+# }
